6c777d6c78b85ea27c17866e74065b7bad9cb903
Camilo Solis <cami.javiers12@gmail.com>
2024-08-03 20:43:27 -0600

    Programa funcional

--- Main.py
+++ Main.py
@@ -1,13 +1,12 @@
 import sys
-
-from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import (QWidget, QMainWindow, QLineEdit, QApplication, QPushButton, QFileDialog, QMessageBox,
-                             QLabel, QPlainTextEdit, QApplication, QDialog, QFileDialog, QAction, QVBoxLayout)
+from PyQt5.QtWidgets import (QMainWindow, QLineEdit,QPushButton, QMessageBox,                             QLabel, QPlainTextEdit, QApplication, QFileDialog)
 from PyQt5.uic import loadUi
+from Stats import StatsForm
+
+
 class UI(QMainWindow):
     def __init__(self):
         super(UI, self).__init__()
-
         loadUi("GUI.ui", self)
 
         # Definiendo los widgets
@@ -22,8 +21,11 @@ class UI(QMainWindow):
         # Acciones de los botones
         self.save_button.clicked.connect(self.save_button_clicked)
         self.open_button.clicked.connect(self.open_button_clicked)
-        #self.statistics_button.clicked.connect(self.statistics_button_clicked)
+        self.statistics_button.clicked.connect(self.statistics_button_clicked)
         self.statistics_button.setEnabled(False)
+
+        self.data = ""
+
     def save_button_clicked(self):
         save_file, _ = QFileDialog.getSaveFileName(self, "Save File", "", "Text Files (*.txt)")
         if save_file:
@@ -37,32 +39,44 @@ class UI(QMainWindow):
                     QMessageBox.about(self, "Save File", "Archivo guardado exitosamente")
                     clear_field = self.txt_content_TextEdit
                     clear_field.clear()
-
                 except Exception as e:
                     QMessageBox.Critical(self, "Save File", f"Ocurrió un error al guardar el archivo {e}")
+
     def open_button_clicked(self):
         # Open file dialog
         file_name, _ = QFileDialog.getOpenFileName(self, "Open File", "", "Text Files (*.txt)")
         if file_name:
             try:
                 with open(file_name, "r") as f:
-                    file_text = f.read()
+                    self.data = f.read()
                     self.path_line_edit.setText(file_name)
-                    self.txt_content_TextEdit.setPlainText(file_text)
+                    self.txt_content_TextEdit.setPlainText(self.data)
                     self.statistics_button.setEnabled(True)
             except Exception as e:
                 QMessageBox.critical(self, "Open File", f"Failed to open file: {e}")
 
-    # def statistics_button_clicked(self):
-    #     try:
-    #         stats_app = StatsForm()
-    #         stats_app.show()
-    #     except Exception as e:
-    #         QMessageBox.critical(self, "Statistics", f"Failed to load {e}")
+    def statistics_button_clicked(self):
+        try:
+            stats = StatsForm()
+            stats.show_total_lines(self.data)
+            stats.show_total_digits(self.data)
+            stats.show_grades_count(self.data)
+            stats.show_total_hyphens(self.data)
+            stats.exec_()
+        except Exception as e:
+            QMessageBox.critical(self, "Statistics", f"Failed to load {e}")
 
 
 # Run the app
 app = QApplication(sys.argv)
 UIWindow = UI()
+# stats = StatsForm()
+# widget = QtWidgets.QStackedWidget()
+# widget.addWidget(UIWindow)
+# widget.addWidget(stats)
 UIWindow.show()
-app.exec_()
+
+try:
+    app.exec_()
+except Exception as e:
+    QMessageBox.critical("Error", f"Failed to run the application: {e}")
--- Stats.py
+++ Stats.py
@@ -1,7 +1,122 @@
-# from PyQt5 import uic, QtWidgets
-#
-# class StatsForm(QtWidgets.QWidget):
-#     def __init__(self):
-#         super().__init__()
-#         uic().loadUi('StatGUI.ui', self)
+import ui
+from PyQt5.uic import loadUi
+from PyQt5.QtWidgets import (QLineEdit, QMessageBox, QDialog)
 
+
+class StatsForm(QDialog):
+    def __init__(self):
+        super().__init__()
+        loadUi("StatGUI.ui", self)
+
+        self.tot_lines_lineEdit = self.findChild(QLineEdit, "tot_lines_lineEdit")
+        self.tot_digits_lineEdit = self.findChild(QLineEdit, "tot_digits_lineEdit")
+        self.tot_grades_lineEdit = self.findChild(QLineEdit, "tot_grades_lineEdit")
+        self.tot_symbol_lineEdit = self.findChild(QLineEdit, "tot_symbol_lineEdit")
+        self.show_st_100_pts_lineEdit = self.findChild(QLineEdit, "show_st_100_pts_lineEdit")
+        self.show_st_0_grade_lineEdit = self.findChild(QLineEdit, "show_st_0_grade_lineEdit")
+        self.show_passed_st_percent_ineEdit = self.findChild(QLineEdit, "show_passed_st_percent_ineEdit")
+        self.show_failed_st_percent_lineEdit = self.findChild(QLineEdit, "show_failed_st_percent_lineEdit")
+        self.show_average_grade_lineEdit = self.findChild(QLineEdit, "show_average_grade_lineEdit")
+
+    def show_total_lines(self, data):
+        try:
+            if not data:
+                QMessageBox.critical(self, "Error", "No se encuentran datos")
+            else:
+                counter = 0
+                for line in data.splitlines():
+                    counter += 1
+                self.tot_lines_lineEdit.setText(str(counter))
+        except Exception as e:
+            QMessageBox.critical(self, "Error en mostrar las lineas", str(e))
+
+    def show_total_digits(self, data):
+        try:
+            if not data:
+                QMessageBox.critical(self, "Error", "No se encuentran datos")
+            else:
+                digitscount = 0
+                for digit in data:
+                    if digit.isdigit():
+                        digitscount += 1
+                self.tot_digits_lineEdit.setText(str(digitscount))
+        except Exception as e:
+            QMessageBox.critical(self, "Error en mostrar los dígitos", str(e))
+
+    def show_grades_count(self, data):
+        try:
+            if not data:
+                QMessageBox.critical(self, "Error", "No se encuentran datos")
+            else:
+                total_sum = 0
+                hundred_count = 0
+                zero_count = 0
+                grade_count = 0
+                current_number = ""
+
+                for grade in data:
+                    if grade.isdigit():
+                        current_number += grade
+                    else:
+                        if current_number:
+                            number = float(current_number)
+                            grade_count += 1
+                            total_sum += number
+                            if number == 0:
+                                zero_count += 1
+                            elif number == 100:
+                                hundred_count += 1
+                            current_number = ""
+
+                if current_number:
+                    number = float(current_number)
+                    grade_count += 1
+                    total_sum += number
+                    if number == 100:
+                        hundred_count += 1
+                    elif number == 0:
+                        zero_count += 1
+
+                if grade_count == 0:
+                    average = 0
+                else:
+                    average = total_sum / grade_count
+
+                passed_percentage = (hundred_count / grade_count) * 100 if grade_count >= 0 else 0
+
+                failed_percentage = (zero_count / grade_count) * 100 if grade_count >= 0 else 0
+
+                self.show_st_100_pts_lineEdit.setText(str(hundred_count))
+                self.show_st_0_grade_lineEdit.setText(str(zero_count))
+                self.show_passed_st_percent_ineEdit.setText(str(round(passed_percentage, 2)))
+                self.show_failed_st_percent_lineEdit.setText(str(round(failed_percentage, 2)))
+                self.show_average_grade_lineEdit.setText(str(round(average, 2)))
+                self.tot_grades_lineEdit.setText(str(grade_count))
+        except Exception as e:
+            QMessageBox.critical(self, "Error en mostrar el conteo de notas", str(e))
+
+    def show_total_hyphens(self, data):
+        try:
+            if not data:
+                QMessageBox.critical(self, "Error", "No se encuentran datos")
+            else:
+                hyphen_count = 0
+                for line in data:
+                    for digit in line:
+                        if digit == '-':
+                            hyphen_count += 1
+                    self.tot_symbol_lineEdit.setText(str(hyphen_count))
+        except Exception as e:
+            QMessageBox.critical(self, "Error en mostrar los '-'", str(e))
+
+    # def show_calculations(self, hundred_count, zero_count, passed_percentage, failed_percentage, average,
+    #                       grade_count):
+    #     try:
+    #         self.show_st_100_pnts_lineEdit(str(hundred_count))
+    #         self.show_st_0_grade_lineEdit(str(zero_count))
+    #         self.show_passed_st_percent_ineEdit(str(passed_percentage))
+    #         self.show_failed_st_percent_lineEdit(str(failed_percentage))
+    #         self.show_average_grade_lineEdit(str(average))
+    #         self.tot_grades_lineEdit.setText(str(grade_count))
+    #     except Exception as e:
+    #         QMessageBox.critical(self, "Error en mostrar los cálculos", str(e))
